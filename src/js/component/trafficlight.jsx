b5ebe9891107a18ab0dc6ff38d6dc8d9bf0444f4
Cornelio Nsue <mastarichy8@gmail.com>
2022-08-10 06:28:48 +0000

    starting css

---
+++ src/js/component/trafficlight.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+const TrafficLight = () => {
+
+    return (
+       <div className="traffict-light">
+            <div className="red light"></div>
+            <div className="yellow light"></div>
+            <div className="green light"></div>
+       </div>
+    )
+}
+
+export default TrafficLight;
