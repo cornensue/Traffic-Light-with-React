422949f3da2384d021928231fc68aaf37e362e4a
Cornelio Nsue <mastarichy8@gmail.com>
2022-08-11 12:08:50 +0000

    almost finished

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
 
 const TrafficLight = () => {
-
-    return (
-       <div className="traffict-light">
-            <div className="red light"></div>
-            <div className="yellow light"></div>
-            <div className="green light"></div>
-       </div>
-    )
+    const [color, setColor] = useState('red');
+    
+    const colors = ['red', 'yellow', 'green'];
+    
+    const red = color === colors[0] ? 'active' : '';
+    const yellow = color === colors[1] ? 'active' : '';
+    const green = color === colors[2] ? 'active' : '';
+    
+    return <>
+            <div className={`traffict-light`}>
+                <div className={`red light ${red}`} onClick={() => setColor(colors[0])}></div>
+                <div className={`yellow light ${yellow}`} onClick={() => setColor(colors[1])}></div>
+                <div className={`green light ${green}`} onClick={() => setColor(colors[2])}></div>
+            </div>
+        </>
 }
 
 export default TrafficLight;
